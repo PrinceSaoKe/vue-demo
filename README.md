d81c8b12be7391d4a9b78ffffe93214ebb11d390
Prince <2596818595@qq.com>
2024-01-05 20:46:03 +0800

    ✨ feat: 实现TodoTable的完成、修改、删除

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * 一些官方文档里用到了好像是ts，有空要了解一下
 * 题目说用导航栏用Banner，但是我没有在ElementPlus里找到这个组件
 * `el-menu`的`background-color`官方文档上说被弃用了，改用`--bg-color`，还没搞清楚要怎么用，写在css里也没效果
+* ElementPlus没有代码补全
--- src/components/Table/TodoTable.vue
+++ src/components/Table/TodoTable.vue
@@ -1,17 +1,53 @@
 <script lang="ts" setup>
 import { useProfileStore } from '@/store/profileStore.js';
 import { useTodoStore } from '@/store/todoStore.js';
+import { reactive, ref } from "vue";
 
 const todoStore = useTodoStore()
 const profileStore = useProfileStore()
 
-function finish(row) {
+const showDialogRef = ref(false)
+
+const updateRow = reactive({
+    id: null,
+    content: null,
+    // finished: null,
+    // createTime: null,
+    // finishTime: null,
+})
+
+function finish(index: number, row) {
     for (const i of todoStore[profileStore['username']]) {
         if (i['id'] === row.id) {
             i['finished'] = true
             return
         }
     }
+}
+
+function showDialog(index: number, row) {
+    updateRow.id = row.id
+    updateRow.content = row.content
+    showDialogRef.value = true
+}
+
+function updateData() {
+    for (const i of todoStore[profileStore['username']]) {
+        if (i['id'] === updateRow['id']) {
+            i['content'] = updateRow['content']
+            showDialogRef.value = false
+            return
+        }
+    }
+}
+
+function deleteData(index: number, row) {
+    todoStore[profileStore['username']].forEach((item, index: number, array) => {
+        if (item.id === row.id) {
+            array.splice(index, 1)
+            return
+        }
+    });
 }
 </script>
 
@@ -22,9 +58,35 @@ function finish(row) {
         <el-table-column label="创建时间" prop="createTime" align="center" />
         <el-table-column label="完成时间" prop="finishTime" align="center" />
         <el-table-column label="操作" fixed="right" align="center">
-            <el-button color="#426440">完成</el-button>
-            <el-button color="#419ADA">修改</el-button>
-            <el-button color="#F81D1D">删除</el-button>
+            <template #default="scope">
+                <el-button color="#426440" @click="finish(scope.$index, scope.row)" v-if="!scope.row.finished">
+                    完成
+                </el-button>
+                <el-button color="#419ADA" v-if="!scope.row.finished" style="color: white;"
+                    @click="showDialog(scope.$index, scope.row)">
+                    修改
+                </el-button>
+                <el-button color="#F81D1D" v-if="!scope.row.finished" @click="deleteData(scope.$index, scope.row)">
+                    删除
+                </el-button>
+                <el-button color="#FFB800" v-if="scope.row.finished" style="color: white;" disabled>已完成</el-button>
+            </template>
         </el-table-column>
     </el-table>
+
+    <el-dialog v-model="showDialogRef">
+        <el-input type="text" v-model="updateRow['content']"></el-input>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="showDialogRef = false">取消</el-button>
+                <el-button type="primary" @click="updateData()">确定</el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
+
+<style scoped>
+.dialog-footer button:first-child {
+    margin-right: 10px;
+}
+</style>
